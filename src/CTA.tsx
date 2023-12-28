9a34ef92700e29a7d7a286b24ce9313046d71ee0
Olasunkanmi Stephen <138253151+olasunkanmiStephen@users.noreply.github.com>
2023-12-28 20:46:52 +0100

    Create CTA.tsx

---
+++ src/CTA.tsx
@@ -0,0 +1,8 @@
+export function CTA() {
+ return (
+  <div id="cta" class=" flex flex-col items-center py-[10vmin] space-y-6">
+   <h4 class="text-white text-center text-4xl">Boost your links today</h4>
+   <button>Get Started</button>
+  </div>
+ );
+}
